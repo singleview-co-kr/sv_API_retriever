ddb45497186b91efff8d77bc118e31db6e5f8c51
singleview-co-kr <w9721066@gmail.com>
2023-04-29 14:38:02 +0900

    Resolve #149
    Add slack.config.ini
    
    modified:   svplugins/slack_clear/task.py

--- svplugins/slack_clear/task.py
+++ svplugins/slack_clear/task.py
@@ -26,6 +26,7 @@
 import os
 import logging
 import sys
+import configparser  # https://docs.python.org/3/library/configparser.html
 from slack_cleaner2.predicates import match
 
 # singleview library
@@ -35,10 +36,12 @@ if __name__ == '__main__': # for console debugging
     import sv_object
     import sv_slack
     import sv_plugin
+    import settings
 else: # for platform running
     from svcommon import sv_object
     from svcommon import sv_slack
     from svcommon import sv_plugin
+    from django.conf import settings
 
 
 class svJobPlugin(sv_object.ISvObject, sv_plugin.ISvPlugin):
@@ -46,28 +49,47 @@ class svJobPlugin(sv_object.ISvObject, sv_plugin.ISvPlugin):
     def __init__(self):
         """ validate dictParams and allocate params to private global attribute """
         s_plugin_name = os.path.abspath(__file__).split(os.path.sep)[-2]
-        self._g_oLogger = logging.getLogger(s_plugin_name+'(20221008)')
+        self._g_oLogger = logging.getLogger(s_plugin_name+'(20230429)')
 
-        self._g_dictParam.update({'slack_ch_ttl':None})
+        self._g_dictParam.update({'slack_ch_ttl': None})
+        self.__g_oConfig = configparser.ConfigParser()
 
     def __del__(self):
         """ never place self._task_post_proc() here 
             __del__() is not executed if try except occurred """
-        pass
+        del self.__g_oConfig
 
     def do_task(self, o_callback):
         self._g_oCallback = o_callback
 
-        if self._g_dictParam['slack_ch_ttl'] is None:
-            self._printDebug('execution denied! -> you need to define slack_ch_ttl')
+        dict_acct_info = self._task_pre_proc(o_callback)
+        if 'sv_account_id' not in dict_acct_info and 'brand_id' not in dict_acct_info and \
+                'nvr_ad_acct' not in dict_acct_info:
+            self._printDebug('stop -> invalid config_loc')
             self._task_post_proc(self._g_oCallback)
             if self._g_bDaemonEnv:  # for running on dbs.py only
                 raise Exception('remove')
             else:
                 return
-        s_slack_ch_title = self._g_dictParam['slack_ch_ttl']
+
+        s_acct_id = dict_acct_info['sv_account_id']
+        s_brand_id = dict_acct_info['brand_id']
+        self.__get_key_config(s_acct_id, s_brand_id)
+
+        s_slack_ch_title = None
+        if 'default' in self.__g_oConfig and 'slack_ch_title' in self.__g_oConfig['default']:
+            s_slack_ch_title = self.__g_oConfig['default']['slack_ch_title']
+        if self._g_dictParam['slack_ch_ttl']:
+            s_slack_ch_title = self._g_dictParam['slack_ch_ttl']
+        elif s_slack_ch_title is None:
+            self._printDebug('execution denied! -> slack_ch_ttl is required via slack.config.ini or slack_ch_ttl param')
+            self._task_post_proc(self._g_oCallback)
+            if self._g_bDaemonEnv:  # for running on dbs.py only
+                raise Exception('remove')
+            else:
+                return
+
         o_sv_slack = sv_slack.SvSlack(sCallingBot='dbs')
-        
         self._printDebug('Prepare to clear slack channel - ' + s_slack_ch_title)
         o_slack_cleaner = o_sv_slack.get_slack_cleaner(s_slack_ch_title)
         # delete all messages in general channels
@@ -83,12 +105,23 @@ class svJobPlugin(sv_object.ISvObject, sv_plugin.ISvPlugin):
         self._printDebug(str(n_cnt) + ' slack msgs have been deleted')
         self._task_post_proc(self._g_oCallback)
 
+    def __get_key_config(self, s_acct_id, s_acct_title):
+        s_config_path = os.path.join(self._g_sAbsRootPath, settings.SV_STORAGE_ROOT, s_acct_id, s_acct_title, 'slack.config.ini')
+        try:
+            with open(s_config_path) as f:
+                self.__g_oConfig.read_file(f)
+        except FileNotFoundError:
+            self._printDebug('slack.config.ini not exist')
+            return  # raise Exception('stop')
+
+        self.__g_oConfig.read(s_config_path)
+
 
 if __name__ == '__main__': # for console debugging and execution
     # python task.py config_loc=1/1 slack_ch_ttl=dbs_bot
     # slack_clear slack_ch_ttl=yuhen_web
     nCliParams = len(sys.argv)
-    if nCliParams > 2:
+    if nCliParams > 1:
         with svJobPlugin() as oJob: # to enforce to call plugin destructor
             oJob.set_my_name('slack_clear')
             oJob.parse_command(sys.argv)
