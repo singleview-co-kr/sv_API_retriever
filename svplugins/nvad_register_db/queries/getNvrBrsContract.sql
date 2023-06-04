a8a5d7ea292f938d48d8a9f05dfed9a9d8263e2c
singleview-co-kr <w9721066@gmail.com>
2023-06-04 16:25:38 +0900

    Commit to #141
    Remove the tbl nvad_brs_contract from job plugin nvad_register_db
    
    deleted:    svplugins/nvad_register_db/queries/getNvrBrsContract.sql
    deleted:    svplugins/nvad_register_db/queries/getNvrBrsContractLastByUa.sql
    deleted:    svplugins/nvad_register_db/schemas/nvad_brs_contract.sql

--- svplugins/nvad_register_db/queries/getNvrBrsContract.sql
+++
@@ -1,3 +0,0 @@
-SELECT `contract_id`, `contract_date_begin`, `contract_date_end`, `contract_amnt`, `refund_amnt`, `ua`
-FROM `nvad_brs_contract`
-WHERE `contract_date_begin` <= %s AND `contract_date_end` >= %s
--- svplugins/nvad_register_db/queries/getNvrBrsContractLastByUa.sql
+++
@@ -1,4 +0,0 @@
-SELECT `contract_date_end`
-FROM `nvad_brs_contract`
-WHERE `ua` = %s
-ORDER BY `contract_date_end` DESC LIMIT 1
